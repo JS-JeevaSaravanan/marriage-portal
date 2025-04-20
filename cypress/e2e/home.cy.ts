18dd482d217c285d8f822619928eaa90d4875e87
JS_JeevaSaravanan <jeevatamil2000@gmail.com>
2025-04-20 13:39:34 +0530

    cypress test updated

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,5 +4,26 @@ describe('Home Page', () => {
       cy.visit('/') // now shorter
       cy.contains('Find Your Perfect Life Partner')
     })
+
+
+    it('navigates to the register page', () => {
+      cy.visit('/')
+      cy.get('a[href="/register"]').first().click()
+      // cy.get('a[href="/register"]').click()
+      cy.url().should('include', '/register')
+      cy.contains('Registration')
+    })
+
+    it('submits a registration form', () => {
+      cy.visit('/register')
+
+      // ToDo: add data-testid to the form fields and remove in production code while building
+      // cy.get('input[name="name"]').type('John Doe')
+      // cy.get('input[name="email"]').type('john@example.com')
+      // cy.get('textarea[name="message"]').type('Hello!')
+      // cy.get('button[type="submit"]').click()
+      // cy.contains('Thank you for your message')
+    })
+    
   })
   
